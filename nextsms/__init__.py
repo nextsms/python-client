29b08f8a0e9a3d2be93906101d7b1ce31e36be40
Kalebu Gwalugano <isaackeinstein@gmail.com>
2021-02-15 16:26:37 +0300

    implemented send_sms()

--- nextsms/__init__.py
+++ nextsms/__init__.py
@@ -1,19 +1,23 @@
 import sys
 import requests
 from base64 import b64encode
+from typing import List, Dict
 from collections import namedtuple
 
 
 class NextSms(object):
 
-    BASE_URL = 'https://messaging-service.co.tz'
+    BASE_URL = ' https://messaging-service.co.tz/api/sms/v1/test/text/single'
 
-    User = namedtuple('User', 'username password secret_kery')
+    User = namedtuple('User', 'username password secret_key')
 
-    def __init__(self, username: str, password: str) -> None:
+    def __init__(self) -> None:
+        self._user = None
+
+    def initialize(self, username: str, password: str) -> None:
         self._user = self.create_user(username, password)
 
-    def create_user(self, username, password):
+    def create_user(self, username, password) -> User:
         if not isinstance(username, str):
             raise TypeError(
                 f"username should be of type <class 'str'> not {type(username)}")
@@ -27,13 +31,41 @@ class NextSms(object):
         )
 
     def create_header(self):
+        if not self._user:
+            raise Exception(
+                '''
+                Please Make sure You initialize before calling any other method
+                
+                >> import nextsms 
+                >> nextsms.initialize(username, password)'''
+            )
+
         return {
-            'Content-Type': 'json',
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
             'Authorization': f'Basic {self._user.secret_key}'
         }
 
-    def send_sms(self):
-        pass
+    def send_sms(self, message: str, recipient: str, sender_id: str = "NEXTSMS"):
+        if not isinstance(sender_id, str):
+            raise TypeError(
+                f"sender_id should of type <class 'str'> not {type(sender_id)}")
+        if not isinstance(recipient, str):
+            raise TypeError(
+                f"recipient should be of type <class 'str'> not {type(recipient)}")
+        if not isinstance(message, str):
+            raise TypeError(
+                f"message should be of type <class 'str'> not {type(message)}")
+
+        return requests.post(
+            self.BASE_URL,
+            headers=self.create_header(),
+            json={
+                'from': sender_id,
+                'to': recipient,
+                'text': message
+            }
+        ).json()
 
 
-sys.modules[__name__] = NextSms
+sys.modules[__name__] = NextSms()
