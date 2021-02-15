422d16b52f701dfa14ac7bfd26538879818cd3b3
Kalebu Gwalugano <isaackeinstein@gmail.com>
2021-02-15 17:21:03 +0300

    added send_bulk()

--- nextsms/__init__.py
+++ nextsms/__init__.py
@@ -1,13 +1,14 @@
 import sys
 import requests
 from base64 import b64encode
-from typing import List, Dict
+from typing import List, Dict, Union
 from collections import namedtuple
 
 
 class NextSms(object):
 
-    BASE_URL = ' https://messaging-service.co.tz/api/sms/v1/test/text/single'
+    BASE_URL_SINGLE = 'https://messaging-service.co.tz/api/sms/v1/test/text/single'
+    BASE_URL_MULTIPLE = 'https://messaging-service.co.tz/api/sms/v1/test/text/multi'
 
     User = namedtuple('User', 'username password secret_key')
 
@@ -30,7 +31,7 @@ class NextSms(object):
             secret_key=b64encode(f'{username}:{password}'.encode()).decode()
         )
 
-    def create_header(self):
+    def create_header(self) -> Dict:
         if not self._user:
             raise Exception(
                 '''
@@ -46,24 +47,41 @@ class NextSms(object):
             'Authorization': f'Basic {self._user.secret_key}'
         }
 
-    def send_sms(self, message: str, recipient: str, sender_id: str = "NEXTSMS"):
+    def send_sms(self, message: str, recipients: Union[str, List], sender_id: str = "NEXTSMS") -> Dict:
         if not isinstance(sender_id, str):
             raise TypeError(
                 f"sender_id should of type <class 'str'> not {type(sender_id)}")
-        if not isinstance(recipient, str):
+        if not isinstance(recipients, (str, list)):
             raise TypeError(
-                f"recipient should be of type <class 'str'> not {type(recipient)}")
+                f"recipient should be of type <class 'str'> or <class 'list'> not {type(recipients)}")
         if not isinstance(message, str):
             raise TypeError(
                 f"message should be of type <class 'str'> not {type(message)}")
 
         return requests.post(
-            self.BASE_URL,
+            self.BASE_URL_SINGLE,
             headers=self.create_header(),
             json={
                 'from': sender_id,
-                'to': recipient,
+                'to': recipients,
                 'text': message
+            }).json()
+
+    def send_bulk(self, messages: List, sender_id: str = "NEXTSMS"):
+
+        if not isinstance(sender_id, str):
+            raise TypeError(
+                f"sender_id should be of type <class 'str'> not {type(sender_id)}")
+
+        if not isinstance(messages, list):
+            raise TypeError(
+                f"messages should be of type <class 'list'> not type {type(messages)}")
+
+        return requests.post(
+            self.BASE_URL_MULTIPLE,
+            headers=self.create_header(),
+            json={
+                'messages': messages
             }
         ).json()
 
